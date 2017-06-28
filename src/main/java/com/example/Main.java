63187d0e919ebab64415733c7ea1155c471fff5e
ofirl <ofirl24@gmail.com>
2017-06-29 00:07:45 +0300

    testing stuff

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -89,8 +89,8 @@ public class Main {
     }
 
     @RequestMapping(value = "/test2", method = RequestMethod.POST)
-    ResponseEntity<String> test2(Map<String, Object> model, @RequestHeader(value="HOST") String host) {
-        //System.out.println("Received POST request:" + json);
+    ResponseEntity<String> test2(Map<String, Object> model,@RequestParam("json") String json, @RequestHeader(value="HOST") String host) {
+        System.out.println("Received POST request:" + json);
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Access-Control-Allow-Origin", "*");
         System.out.println("Received POST request from:" + host);
