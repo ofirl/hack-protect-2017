c694883f00fdf31c5c8f34288f7c6574e270d28d
ofirl <ofirl24@gmail.com>
2017-06-28 11:29:38 +0300

    initial db stuff

---
+++ src/main/java/Database/SqlHandler.java
@@ -0,0 +1,21 @@
+package Database;
+
+import java.util.*;
+
+public class SqlHandler {
+    public void upadte(String table, Dictionary<String, String> values, String condition) {
+
+    }
+
+    public void insert(String table, Dictionary<String, String> values) {
+
+    }
+
+    public void delete(String table, String condition) {
+
+    }
+
+    public void select(String table, List<String> fields, String condition) {
+
+    }
+}
