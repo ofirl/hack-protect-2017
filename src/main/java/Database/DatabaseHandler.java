a045a90589f1d8e4465aa7a420c70e88674237c6
Shellynagar <shellynagar30@gmail.com>
2017-06-28 17:30:28 +0300

    write handle title

--- src/main/java/Database/DatabaseHandler.java
+++ src/main/java/Database/DatabaseHandler.java
@@ -2,7 +2,9 @@ package Database;
 
 import DataClasses.*;
 
+import java.sql.Array;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 public class DatabaseHandler {
@@ -31,10 +33,26 @@ public class DatabaseHandler {
 
     // will search the db for titles with the same words in the same order
     // will return <found title length> / <title length>
-    public static double[] searchTitle(String[] title) {
-        ResultSet rs = SqlHandler.select("Titles", new String [] {"headline"}, "headline LIKE");
-
-        return null;
+    public static Map<String,Double> searchTitle(String[] title) throws SQLException {
+        HashMap<String, Double> values = new HashMap<>();
+        ResultSet rs = SqlHandler.select("Titles", new String[]{"id", "headline"}, "headline LIKE");
+        double max = 0;
+        String id = "";
+        while(rs.next()) {
+            String[] dbTitle = (String[]) rs.getArray("headline").getArray();
+            int index = 0;
+            for (int i = 0; i < title.length; i++) {
+                if (dbTitle[i].equals(title[index]))
+                    index++;
+            }
+            double reliability = index / title.length;
+            if (max < reliability) {
+                max = reliability;
+                id = rs.getString("id");
+            }
+            values.put(rs.getString("id"), reliability);
+        }
+        return values;
     }
 
     public static String processCrawlRequest(Map<Integer, Article> request, String domain) {
